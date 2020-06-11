45aa030a23f2fea88937ba380119558de0291215
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2020-06-11 10:51:31 +0700

    edit: sql connection

--- config/connectdb.go
+++ config/connectdb.go
@@ -18,7 +18,8 @@ func ConnectToDB() {
 	dbUser := "root"
 	dbPass := "root"
 	dbName := "thesis"
-	DB, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName+"?parseTime=true")
+	//DB, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName+"?parseTime=true")
+	DB, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(159.89.202.223:3306)/"+dbName+"?parseTime=true")
 	if err != nil {
 		panic(err.Error())
 	}
