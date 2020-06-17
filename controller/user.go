39913308e2cb8320e6c42e02142da713a953c9db
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2020-06-17 12:43:05 +0700

    add to log

--- controller/user.go
+++ controller/user.go
@@ -357,6 +357,8 @@ func LoginUserWithPassword(c *gin.Context) {
 	err = config.DB.QueryRow("SELECT id, email, password FROM login WHERE email=?", user.Email).Scan(&databaseID, &databaseEmail, &databasePassword)
 
 	if err != nil {
+		fmt.Println("user email", user.Email)
+		fmt.Println("user pass", user.Password)
 		fmt.Println("err", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"code":    http.StatusInternalServerError,
