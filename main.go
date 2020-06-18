b62f353be3227022cc9b52a67a303ebfd6e1c6da
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2020-06-18 17:41:37 +0700

    add: abortstatus when method options

--- main.go
+++ main.go
@@ -119,6 +119,11 @@ func Cors() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, token")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		c.Next()
 	}
 }
