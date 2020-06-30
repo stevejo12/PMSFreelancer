be690853694eaef074c20790ab135bfe4516e501
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2020-06-30 18:12:23 +0700

    change: data type struct moota

--- models/payment.go
+++ models/payment.go
@@ -4,10 +4,10 @@ package models
 type GetTransactionMutationRequest struct {
 	ID            string `json:"id"`
 	BankID        string `json:"bank_id"`
-	AccountNumber int    `json:"account_number"`
+	AccountNumber string `json:"account_number"`
 	BankType      string `json:"bank_type"`
 	Date          string `json:"date"`
-	Amount        string `json:"amount"`
+	Amount        int    `json:"amount"`
 	Description   string `json:"description"`
 	Type          string `json:"type"`
 	Balance       int    `json:"balance"`
