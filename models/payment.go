b4ca382a31856e61c13a6378e5d5588fea380e38
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2020-06-30 19:16:16 +0700

    fix: change balance data type to float64

--- models/payment.go
+++ models/payment.go
@@ -2,15 +2,15 @@ package models
 
 // GetTransactionMutationRequest func
 type GetTransactionMutationRequest struct {
-	ID            string `json:"id"`
-	BankID        string `json:"bank_id"`
-	AccountNumber string `json:"account_number"`
-	BankType      string `json:"bank_type"`
-	Date          string `json:"date"`
-	Amount        int    `json:"amount"`
-	Description   string `json:"description"`
-	Type          string `json:"type"`
-	Balance       int    `json:"balance"`
+	ID            string  `json:"id"`
+	BankID        string  `json:"bank_id"`
+	AccountNumber string  `json:"account_number"`
+	BankType      string  `json:"bank_type"`
+	Date          string  `json:"date"`
+	Amount        int     `json:"amount"`
+	Description   string  `json:"description"`
+	Type          string  `json:"type"`
+	Balance       float64 `json:"balance"`
 }
 
 type DepositParameter struct {
